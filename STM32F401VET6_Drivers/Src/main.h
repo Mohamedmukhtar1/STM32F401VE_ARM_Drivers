7b7d1cadd364e05d319906febe55eeac5a8f2148
Mohamedmukhtar1 <muhamdmokhtar1@gmail.com>
2022-05-18 19:27:21 +0200

    update main

---
+++ STM32F401VET6_Drivers/Src/main.h
@@ -0,0 +1,23 @@
+/*
+ * main.h
+ *
+ *  Created on: May 13, 2022
+ *      Author: Eng- Mohamed Mokhtar
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/***************_APP_INCLUDES_*****************************/
+#include "APP_Utility.h"
+
+/***************_MyApp_INCLUDES_***************************/
+
+/***************_HAL_INCLUDES_*****************************/
+
+/***************_MCAL_INCLUDES_****************************/
+#include "MCAL_RCC_Interface.h"
+#include "MCAL_GPIO_Interface.h"
+/***************_main.c_FUNCTION_PROTOTYPE_****************/
+
+#endif /* MAIN_H_ */
