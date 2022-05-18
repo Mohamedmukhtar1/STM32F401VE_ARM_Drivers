2498d422fa0dd2a8a1ef281891e1d78243560a6f
Mohamedmukhtar1 <muhamdmokhtar1@gmail.com>
2022-05-18 23:03:38 +0200

    Updated GPIO driver

--- STM32F401VET6_Drivers/Inc/Inc_MCAL/GPIO/MCAL_GPIO_Interface.h
+++ STM32F401VET6_Drivers/Inc/Inc_MCAL/GPIO/MCAL_GPIO_Interface.h
@@ -16,74 +16,74 @@
 
 /***************_MACROS_CONFIGURATION_*********************/
 /*======== Macros Of Port ========*/
-#define GPIOA_PORT   		      (0U)            /*!< GPIO (A) PORT  */
-#define GPIOB_PORT   		      (1U)            /*!< GPIO (B) PORT  */
-#define GPIOC_PORT   		      (2U)            /*!< GPIO (C) PORT  */
-#define GPIOD_PORT   		      (3U)            /*!< GPIO (D) PORT  */
-#define GPIOE_PORT   		      (4U)            /*!< GPIO (E) PORT  */
-#define GPIOH_PORT   		      (5U)            /*!< GPIO (H) PORT  */
+#define GPIOA_PORT   		      ((U8)0U)            /*!< GPIO (A) PORT  */
+#define GPIOB_PORT   		      ((U8)1U)            /*!< GPIO (B) PORT  */
+#define GPIOC_PORT   		      ((U8)2U)            /*!< GPIO (C) PORT  */
+#define GPIOD_PORT   		      ((U8)3U)            /*!< GPIO (D) PORT  */
+#define GPIOE_PORT   		      ((U8)4U)            /*!< GPIO (E) PORT  */
+#define GPIOH_PORT   		      ((U8)5U)            /*!< GPIO (H) PORT  */
 
 /*======== Macros Of Pins ========*/
-#define GPIO_PIN_0              ((u16)0U )       /*!< Pin 0 selected    */
-#define GPIO_PIN_1              ((u16)1U )       /*!< Pin 1 selected    */
-#define GPIO_PIN_2              ((u16)2U )       /*!< Pin 2 selected    */
-#define GPIO_PIN_3              ((u16)3U )       /*!< Pin 3 selected    */
-#define GPIO_PIN_4              ((u16)4U )       /*!< Pin 4 selected    */
-#define GPIO_PIN_5              ((u16)5U )       /*!< Pin 5 selected    */
-#define GPIO_PIN_6              ((u16)6U )       /*!< Pin 6 selected    */
-#define GPIO_PIN_7              ((u16)7U )       /*!< Pin 7 selected    */
-#define GPIO_PIN_8              ((u16)8U )       /*!< Pin 8 selected    */
-#define GPIO_PIN_9              ((u16)9U )       /*!< Pin 9 selected    */
-#define GPIO_PIN_10             ((u16)10U)       /*!< Pin 10 selected   */
-#define GPIO_PIN_11             ((u16)11U)       /*!< Pin 11 selected   */
-#define GPIO_PIN_12             ((u16)12U)       /*!< Pin 12 selected   */
-#define GPIO_PIN_13             ((u16)13U)       /*!< Pin 13 selected   */
-#define GPIO_PIN_14             ((u16)14U)       /*!< Pin 14 selected   */
-#define GPIO_PIN_15             ((u16)15U)       /*!< Pin 15 selected   */
-#define GPIO_PIN_All            ((u16)0xFFFF)    /*!< All pins selected */
+#define GPIO_PIN_0              ((U8)0U)       /*!< Pin 0 selected    */
+#define GPIO_PIN_1              ((U8)1U)       /*!< Pin 1 selected    */
+#define GPIO_PIN_2              ((U8)2U)       /*!< Pin 2 selected    */
+#define GPIO_PIN_3              ((U8)3U)       /*!< Pin 3 selected    */
+#define GPIO_PIN_4              ((U8)4U)       /*!< Pin 4 selected    */
+#define GPIO_PIN_5              ((U8)5U)       /*!< Pin 5 selected    */
+#define GPIO_PIN_6              ((U8)6U)       /*!< Pin 6 selected    */
+#define GPIO_PIN_7              ((U8)7U)       /*!< Pin 7 selected    */
+#define GPIO_PIN_8              ((U8)8U)       /*!< Pin 8 selected    */
+#define GPIO_PIN_9              ((U8)9U)       /*!< Pin 9 selected    */
+#define GPIO_PIN_10             ((U8)10U)       /*!< Pin 10 selected   */
+#define GPIO_PIN_11             ((U8)11U)       /*!< Pin 11 selected   */
+#define GPIO_PIN_12             ((U8)12U)       /*!< Pin 12 selected   */
+#define GPIO_PIN_13             ((U8)13U)       /*!< Pin 13 selected   */
+#define GPIO_PIN_14             ((U8)14U)       /*!< Pin 14 selected   */
+#define GPIO_PIN_15             ((U8)15U)       /*!< Pin 15 selected   */
+//#define GPIO_PIN_All            ((U16)0xFFFF)    /*!< All pins selected */  ####################
 
-/*======== Macros Of Mode ========*/
-#define INPUT_MODE  	      (0b00)          /*!< MODE ---> INPUT     */
-#define OUTPUT_MODE   	      (0b01)          /*!< MODE ---> OUTPUT    */
-#define ALTF_MODE   	      (0b10)          /*!< MODE ---> ALTF      */
-#define ANALOG_MODE   	      (0b11)          /*!< MODE ---> ANALOG    */
+/*======== Macros Of Pin Mode ========*/
+#define INPUT_MODE  	         ((U8)0U)          /*!< MODE ---> INPUT     */
+#define OUTPUT_MODE   	         ((U8)1U)          /*!< MODE ---> OUTPUT    */
+#define ALTERNATE_FUNCTION_MODE  ((U8)2U)          /*!< MODE ---> ALTF      */
+#define ANALOG_MODE   	         ((U8)3U)          /*!< MODE ---> ANALOG    */
 
 /*======== Macros Of Output Type ========*/
-#define OP_TYPE_PUSH_PULL    (0U)             /*!< OUTPUT TYPE ---> PUSH PULL   */
-#define OP_TYPE_OPEN_DRAIN   (1U)             /*!< OUTPUT TYPE ---> OPEN DRAIN  */
-/*======== Macros Of Speed ========*/
+#define OUTPUT_PUSH_PULL_TYPE    ((U8)0U)          /*!< OUTPUT TYPE ---> PUSH PULL   */
+#define OUTPUT_OPEN_DRAIN_TYPE   ((U8)1U)          /*!< OUTPUT TYPE ---> OPEN DRAIN  */
+/*======== Macros Of Pin Speed ========*/
 
-#define OUTPUT_SPEED_LOW         (0x00000000U)  /*!< IO works at 2 MHz, please refer to the product datasheet */
-#define OUTPUT_SPEED_MEDUIM      (0x00000001U)  /*!< range 12,5 MHz to 50 MHz, please refer to the product datasheet */
-#define OUTPUT_SPEED_HIGH        (0x00000002U)  /*!< range 25 MHz to 100 MHz, please refer to the product datasheet  */
-#define OUTPUT_SPEED_VERY_HIGH   (0x00000003U)  /*!< range 50 MHz to 200 MHz, please refer to the product datasheet  */
+#define OUTPUT_LOW_SPEED        ((U8)0U)  /*!< IO works at 2 MHz, please refer to the product datasheet */
+#define OUTPUT_MEDUIM_SPEED     ((U8)1U)  /*!< range 12,5 MHz to 50 MHz, please refer to the product datasheet */
+#define OUTPUT_HIGH_SPEED       ((U8)2U)  /*!< range 25 MHz to 100 MHz, please refer to the product datasheet  */
+#define OUTPUT_VERY_HIGH_SPEED  ((U8)3U)  /*!< range 50 MHz to 200 MHz, please refer to the product datasheet  */
 
-/*======== Macros Of PULL  ========*/
-#define PULL_OFF_STATE                 (0x00000000U)   /*!< No Pull-up or Pull-down activation  */
-#define PULL_UP_STATE                  (0x00000001U)   /*!< Pull-up activation                  */
-#define PULL_DOWN_STATE                (0x00000002U)   /*!< Pull-down activation                */
+/*======== Macros Of Pin PULL  ========*/
+#define PULL_OFF_STATE         ((U8)0U)   /*!< No Pull-up or Pull-down activation  */
+#define PULL_UP_STATE                  ((U8)1U)   /*!< Pull-up activation                  */
+#define PULL_DOWN_STATE                ((U8)2U)   /*!< Pull-down activation                */
 
 /*======== Macros Write Data  ========*/
-#define LOW_STATE                    (0U)
-#define HIGH_STATE	                 (1U)
+#define LOW_STATE                    ((U32)0U)
+#define HIGH_STATE	                 ((U32)1U)
 
 /*======== Macros ALTF  ========*/
-#define ALTFN_0                  (0b0000)      /*!< ALTF  --> 0  */
-#define ALTFN_1                  (0b0001)      /*!< ALTF  --> 1  */
-#define ALTFN_2                  (0b0010)      /*!< ALTF  --> 2  */
-#define ALTFN_3                  (0b0011)      /*!< ALTF  --> 3  */
-#define ALTFN_4                  (0b0100)      /*!< ALTF  --> 4  */
-#define ALTFN_5                  (0b0101)      /*!< ALTF  --> 5  */
-#define ALTFN_6                  (0b0110)      /*!< ALTF  --> 6  */
-#define ALTFN_7                  (0b0111)      /*!< ALTF  --> 7  */
-#define ALTFN_8                  (0b1000)      /*!< ALTF  --> 8  */
-#define ALTFN_9                  (0b1001)      /*!< ALTF  --> 9  */
-#define ALTFN_10                 (0b1010)      /*!< ALTF  --> 10 */
-#define ALTFN_11                 (0b1011)      /*!< ALTF  --> 11 */
-#define ALTFN_12                 (0b1100)      /*!< ALTF  --> 12 */
-#define ALTFN_13                 (0b1101)      /*!< ALTF  --> 13 */
-#define ALTFN_14                 (0b1110)      /*!< ALTF  --> 14 */
-#define ALTFN_15                 (0b1111)      /*!< ALTF  --> 15 */
+#define ALTFUN_0                  (0b0000)      /*!< ALTF  --> 0  */
+#define ALTFUN_1                  (0b0001)      /*!< ALTF  --> 1  */
+#define ALTFUN_2                  (0b0010)      /*!< ALTF  --> 2  */
+#define ALTFUN_3                  (0b0011)      /*!< ALTF  --> 3  */
+#define ALTFUN_4                  (0b0100)      /*!< ALTF  --> 4  */
+#define ALTFUN_5                  (0b0101)      /*!< ALTF  --> 5  */
+#define ALTFUN_6                  (0b0110)      /*!< ALTF  --> 6  */
+#define ALTFUN_7                  (0b0111)      /*!< ALTF  --> 7  */
+#define ALTFUN_8                  (0b1000)      /*!< ALTF  --> 8  */
+#define ALTFUN_9                  (0b1001)      /*!< ALTF  --> 9  */
+#define ALTFUN_10                 (0b1010)      /*!< ALTF  --> 10 */
+#define ALTFUN_11                 (0b1011)      /*!< ALTF  --> 11 */
+#define ALTFUN_12                 (0b1100)      /*!< ALTF  --> 12 */
+#define ALTFUN_13                 (0b1101)      /*!< ALTF  --> 13 */
+#define ALTFUN_14                 (0b1110)      /*!< ALTF  --> 14 */
+#define ALTFUN_15                 (0b1111)      /*!< ALTF  --> 15 */
 
 
 /*============================================================================
@@ -91,8 +91,9 @@
   ============================================================================*/
 void MGPIO_vSetPinMode(U8 PortName ,U8 PinNum ,U8 DirectionMode);
 void MGPIO_vSetPinOutPutType(U8 PortName ,U8 PinNum ,U8 OutPutType);
-void MGPIO_vSetPinOutSpeed(U8 Copy_u8PortName ,U32 Copy_u8PinNum ,U32 Copy_u8OutSpeed );
-void MGPIO_vSetPullType(U8 Copy_u8PortName ,U32 Copy_u8PinNum ,U32 Copy_u8PullType );
+void MGPIO_vSetPinOutSpeed(U8 Copy_u8PortName ,U8 Copy_u8PinNum ,U8 Copy_u8OutSpeed );
+void MGPIO_vSetPullType(U8 Copy_u8PortName ,U8 Copy_u8PinNum ,U8 Copy_u8PullType );
+U8 MGPIO_U8PinReadData(U8 PortName ,U8 PinNum);
 
 
 #endif /* INC_MCAL_GPIO_MCAL_GPIO_INTERFACE_H_ */
--- STM32F401VET6_Drivers/Src/Src_MCAL/GPIO/MCAL_GPIO.c
+++ STM32F401VET6_Drivers/Src/Src_MCAL/GPIO/MCAL_GPIO.c
@@ -9,11 +9,11 @@
 
 void MGPIO_vSetPinMode(U8 PortName ,U8 PinNum ,U8 DirectionMode)
 {
+	/* INPUT_MODE, OUTPUT_MODE, ALTERNATE_FUNCTION_MODE, ANALOG_MODE */
 	switch(PortName)
 	{
-		case GPIOA_PORT :
-			GIVE_BIT(GPIOA_SPTR->GPIO_MODER.RegisterAccess, 2U* PinNum, (U32)DirectionMode);
-			break;
+	    //GIVE_BIT(GPIOA_SPTR->GPIO_MODER.RegisterAccess, 2U* PinNum, (U32)DirectionMode) ; break;
+		case GPIOA_PORT :GPIOA_SPTR->GPIO_MODER.RegisterAccess |= (U32)(DirectionMode<<(2U* PinNum)) ; break;
 		case GPIOB_PORT :GPIOB_SPTR->GPIO_MODER.RegisterAccess |= (U32)(DirectionMode<<(2U* PinNum)) ; break;
 		case GPIOC_PORT :GPIOC_SPTR->GPIO_MODER.RegisterAccess |= (U32)(DirectionMode<<(2U* PinNum)) ; break;
 		case GPIOD_PORT :GPIOD_SPTR->GPIO_MODER.RegisterAccess |= (U32)(DirectionMode<<(2U* PinNum)) ; break;
@@ -23,45 +23,164 @@ void MGPIO_vSetPinMode(U8 PortName ,U8 PinNum ,U8 DirectionMode)
     }
 }
 
-void MGPIO_vSetPinOutPutType(U8 PortName ,U8 PinNum ,U8 OutPutType)
+void MGPIO_vSetPinOutputType(U8 PortName ,U8 PinNum ,U8 OutPutType)
 {
+	/*OUTPUT_PUSH_PULL_TYPE, OUTPUT_OPEN_DRAIN_TYPE*/
 	switch(PortName)
 	{
-		case GPIOA_PORT :GPIOA_SPTR->GPIO_OTYPER.RegisterAccess |= (U32)(OutPutType<<(2U* PinNum)) ; break;
-		case GPIOB_PORT :GPIOB_SPTR->GPIO_OTYPER.RegisterAccess |= (U32)(OutPutType<<(2U* PinNum)) ; break;
-		case GPIOC_PORT :GPIOC_SPTR->GPIO_OTYPER.RegisterAccess |= (U32)(OutPutType<<(2U* PinNum)) ; break;
-		case GPIOD_PORT :GPIOD_SPTR->GPIO_OTYPER.RegisterAccess |= (U32)(OutPutType<<(2U* PinNum)) ; break;
-		case GPIOE_PORT :GPIOE_SPTR->GPIO_OTYPER.RegisterAccess |= (U32)(OutPutType<<(2U* PinNum)) ; break;
-		case GPIOH_PORT :GPIOH_SPTR->GPIO_OTYPER.RegisterAccess |= (U32)(OutPutType<<(2U* PinNum)) ; break;
+		case GPIOA_PORT :GPIOA_SPTR->GPIO_OTYPER.RegisterAccess |= (U32)(OutPutType<<(PinNum)) ; break;
+		case GPIOB_PORT :GPIOB_SPTR->GPIO_OTYPER.RegisterAccess |= (U32)(OutPutType<<(PinNum)) ; break;
+		case GPIOC_PORT :GPIOC_SPTR->GPIO_OTYPER.RegisterAccess |= (U32)(OutPutType<<(PinNum)) ; break;
+		case GPIOD_PORT :GPIOD_SPTR->GPIO_OTYPER.RegisterAccess |= (U32)(OutPutType<<(PinNum)) ; break;
+		case GPIOE_PORT :GPIOE_SPTR->GPIO_OTYPER.RegisterAccess |= (U32)(OutPutType<<(PinNum)) ; break;
+		case GPIOH_PORT :GPIOH_SPTR->GPIO_OTYPER.RegisterAccess |= (U32)(OutPutType<<(PinNum)) ; break;
 		default : /*ERROR*/ break ;
      }
 }
 
-void MGPIO_vSetPinOutSpeed(U8 Copy_u8PortName ,U32 Copy_u8PinNum ,U32 Copy_u8OutSpeed )
+void MGPIO_vSetPinOutputSpeed(U8 PortName ,U8 PinNum ,U8 OutputSpeed )
 {
-	switch(Copy_u8PortName)
+	/*OUTPUT_LOW_SPEED, OUTPUT_MEDUIM_SPEED, OUTPUT_HIGH_SPEED, OUTPUT_VERY_HIGH_SPEED*/
+	switch(PortName)
 	{
-	  case GPIOA_PORT :GPIOA_SPTR->GPIO_OSPEEDR.RegisterAccess |= (U32)(Copy_u8OutSpeed<<(2U* Copy_u8PinNum)) ; break;
-	  case GPIOB_PORT :GPIOB_SPTR->GPIO_OSPEEDR.RegisterAccess |= (U32)(Copy_u8OutSpeed<<(2U* Copy_u8PinNum)) ; break;
-	  case GPIOC_PORT :GPIOC_SPTR->GPIO_OSPEEDR.RegisterAccess |= (U32)(Copy_u8OutSpeed<<(2U* Copy_u8PinNum)) ; break;
-	  case GPIOD_PORT :GPIOD_SPTR->GPIO_OSPEEDR.RegisterAccess |= (U32)(Copy_u8OutSpeed<<(2U* Copy_u8PinNum)) ; break;
-	  case GPIOE_PORT :GPIOE_SPTR->GPIO_OSPEEDR.RegisterAccess |= (U32)(Copy_u8OutSpeed<<(2U* Copy_u8PinNum)) ; break;
-	  case GPIOH_PORT :GPIOH_SPTR->GPIO_OSPEEDR.RegisterAccess |= (U32)(Copy_u8OutSpeed<<(2U* Copy_u8PinNum)) ; break;
+	  case GPIOA_PORT :GPIOA_SPTR->GPIO_OSPEEDR.RegisterAccess |= (U32)(OutputSpeed<<(2U* PinNum)) ; break;
+	  case GPIOB_PORT :GPIOB_SPTR->GPIO_OSPEEDR.RegisterAccess |= (U32)(OutputSpeed<<(2U* PinNum)) ; break;
+	  case GPIOC_PORT :GPIOC_SPTR->GPIO_OSPEEDR.RegisterAccess |= (U32)(OutputSpeed<<(2U* PinNum)) ; break;
+	  case GPIOD_PORT :GPIOD_SPTR->GPIO_OSPEEDR.RegisterAccess |= (U32)(OutputSpeed<<(2U* PinNum)) ; break;
+	  case GPIOE_PORT :GPIOE_SPTR->GPIO_OSPEEDR.RegisterAccess |= (U32)(OutputSpeed<<(2U* PinNum)) ; break;
+	  case GPIOH_PORT :GPIOH_SPTR->GPIO_OSPEEDR.RegisterAccess |= (U32)(OutputSpeed<<(2U* PinNum)) ; break;
 	  default : /*ERROR*/ break ;
 	}
 }
 
-void MGPIO_vSetPullType(U8 Copy_u8PortName ,U32 Copy_u8PinNum ,U32 Copy_u8PullType )
+void MGPIO_vSetPinPullType(U8 PortName ,U8 PinNum ,U8 PullType )
 {
-	switch(Copy_u8PortName)
+	/*PULL_OFF_STATE, PULL_UP_STATE, PULL_DOWN_STATE*/
+	switch(PortName)
 	{
-	  case GPIOA_PORT :GPIOA_SPTR->GPIO_PUPDR.RegisterAccess |= (U32) ( Copy_u8PullType << ( 2U* Copy_u8PinNum )) ; break;
-	  case GPIOB_PORT :GPIOB_SPTR->GPIO_PUPDR.RegisterAccess |= (U32) ( Copy_u8PullType << ( 2U* Copy_u8PinNum )) ; break;
-	  case GPIOC_PORT :GPIOC_SPTR->GPIO_PUPDR.RegisterAccess |= (U32) ( Copy_u8PullType << ( 2U* Copy_u8PinNum )) ; break;
-	  case GPIOD_PORT :GPIOD_SPTR->GPIO_PUPDR.RegisterAccess |= (U32) ( Copy_u8PullType << ( 2U* Copy_u8PinNum )) ; break;
-	  case GPIOE_PORT :GPIOE_SPTR->GPIO_PUPDR.RegisterAccess |= (U32) ( Copy_u8PullType << ( 2U* Copy_u8PinNum )) ; break;
-	  case GPIOH_PORT :GPIOH_SPTR->GPIO_PUPDR.RegisterAccess |= (U32) ( Copy_u8PullType << ( 2U* Copy_u8PinNum )) ; break;
+	  case GPIOA_PORT :GPIOA_SPTR->GPIO_PUPDR.RegisterAccess |= (U32)(PullType<<(2U* PinNum)) ; break;
+	  case GPIOB_PORT :GPIOB_SPTR->GPIO_PUPDR.RegisterAccess |= (U32)(PullType<<(2U* PinNum)) ; break;
+	  case GPIOC_PORT :GPIOC_SPTR->GPIO_PUPDR.RegisterAccess |= (U32)(PullType<<(2U* PinNum)) ; break;
+	  case GPIOD_PORT :GPIOD_SPTR->GPIO_PUPDR.RegisterAccess |= (U32)(PullType<<(2U* PinNum)) ; break;
+	  case GPIOE_PORT :GPIOE_SPTR->GPIO_PUPDR.RegisterAccess |= (U32)(PullType<<(2U* PinNum)) ; break;
+	  case GPIOH_PORT :GPIOH_SPTR->GPIO_PUPDR.RegisterAccess |= (U32)(PullType<<(2U* PinNum)) ; break;
 	  default : /*ERROR*/ break ;
 	}
 }
 
+U8 MGPIO_U8ReadPinData(U8 PortName ,U8 PinNum)
+{
+	U8 L_U8Pin_Read_Data =0;
+	switch(PortName)
+	{
+	  case GPIOA_PORT :L_U8Pin_Read_Data = GET_BIT(GPIOA_SPTR->GPIO_IDR.RegisterAccess, PinNum) ; break;
+	  case GPIOB_PORT :L_U8Pin_Read_Data = GET_BIT(GPIOA_SPTR->GPIO_IDR.RegisterAccess, PinNum) ; break;
+	  case GPIOC_PORT :L_U8Pin_Read_Data = GET_BIT(GPIOA_SPTR->GPIO_IDR.RegisterAccess, PinNum) ; break;
+	  case GPIOD_PORT :L_U8Pin_Read_Data = GET_BIT(GPIOA_SPTR->GPIO_IDR.RegisterAccess, PinNum) ; break;
+	  case GPIOE_PORT :L_U8Pin_Read_Data = GET_BIT(GPIOA_SPTR->GPIO_IDR.RegisterAccess, PinNum) ; break;
+	  case GPIOH_PORT :L_U8Pin_Read_Data = GET_BIT(GPIOA_SPTR->GPIO_IDR.RegisterAccess, PinNum) ; break;
+	  default : /*ERROR*/ break ;
+	}
+	return L_U8Pin_Read_Data;
+}
+
+U16 MGPIO_U16ReadPortData(U8 PortName)
+{
+	U8 L_U8Pin_Read_Data =0;
+	switch(PortName)
+	{
+	  case GPIOA_PORT :L_U8Pin_Read_Data = GET_REG(GPIOA_SPTR->GPIO_IDR.RegisterAccess) ; break;
+	  case GPIOB_PORT :L_U8Pin_Read_Data = GET_REG(GPIOA_SPTR->GPIO_IDR.RegisterAccess) ; break;
+	  case GPIOC_PORT :L_U8Pin_Read_Data = GET_REG(GPIOA_SPTR->GPIO_IDR.RegisterAccess) ; break;
+	  case GPIOD_PORT :L_U8Pin_Read_Data = GET_REG(GPIOA_SPTR->GPIO_IDR.RegisterAccess) ; break;
+	  case GPIOE_PORT :L_U8Pin_Read_Data = GET_REG(GPIOA_SPTR->GPIO_IDR.RegisterAccess) ; break;
+	  case GPIOH_PORT :L_U8Pin_Read_Data = GET_REG(GPIOA_SPTR->GPIO_IDR.RegisterAccess) ; break;
+	  default : /*ERROR*/ break ;
+	}
+	return L_U8Pin_Read_Data;
+}
+
+void MGPIO_vWritePinData(U8 PortName ,U8 PinNum, U8 OutputState)
+{
+	/*LOW_STATE, HIGH_STATE*/
+	switch(PortName)
+	{
+	    case GPIOA_PORT :GPIOA_SPTR->GPIO_ODR.RegisterAccess |= (U32)(OutputState<<(PinNum)) ; break;
+	    case GPIOB_PORT :GPIOB_SPTR->GPIO_ODR.RegisterAccess |= (U32)(OutputState<<(PinNum)) ; break;
+	    case GPIOC_PORT :GPIOC_SPTR->GPIO_ODR.RegisterAccess |= (U32)(OutputState<<(PinNum)) ; break;
+	    case GPIOD_PORT :GPIOD_SPTR->GPIO_ODR.RegisterAccess |= (U32)(OutputState<<(PinNum)) ; break;
+	    case GPIOE_PORT :GPIOE_SPTR->GPIO_ODR.RegisterAccess |= (U32)(OutputState<<(PinNum)) ; break;
+	    case GPIOH_PORT :GPIOH_SPTR->GPIO_ODR.RegisterAccess |= (U32)(OutputState<<(PinNum)) ; break;
+		default : /*ERROR*/ break ;
+	}
+}
+
+
+void MGPIO_vPinLock(U8 PortNum, U8 PinNum )
+{
+	switch(PortNum)
+	{
+	  case _GPIOA_PORT  :
+		  SET_BIT(GPIOA_SPTR->GPIO_LCKR.RegisterAccess, (U32)(PinNum)) ; break;
+		  SET_BIT(GPIOA_SPTR->GPIO_LCKR.RegisterAccess, (U32)(16U))    ; break;
+		  while(!(GPIOA_SPTR->GPIO_LCKR.RegisterAccess, (U32)(16U)));
+		  break ;
+	  case _GPIOB_PORT  :
+		  SET_BIT(GPIOB_SPTR->GPIO_LCKR.RegisterAccess, (U32)(PinNum)) ; break;
+		  SET_BIT(GPIOB_SPTR->GPIO_LCKR.RegisterAccess, (U32)(16U))    ; break;
+		  while(!(GPIOB_SPTR->GPIO_LCKR.RegisterAccess, (U32)(16U)));
+		  break ;
+	  case _GPIOC_PORT  :
+		  SET_BIT(GPIOC_SPTR->GPIO_LCKR.RegisterAccess, (U32)(PinNum)) ; break;
+		  SET_BIT(GPIOC_SPTR->GPIO_LCKR.RegisterAccess, (U32)(16U))    ; break;
+		  while(!(GPIOC_SPTR->GPIO_LCKR.RegisterAccess, (U32)(16U)));
+		  break ;
+	  case _GPIOD_PORT  :
+		  SET_BIT(GPIOD_SPTR->GPIO_LCKR.RegisterAccess, (U32)(PinNum)) ; break;
+		  SET_BIT(GPIOD_SPTR->GPIO_LCKR.RegisterAccess, (U32)(16U))    ; break;
+		  while(!(GPIOD_SPTR->GPIO_LCKR.RegisterAccess, (U32)(16U)));
+		  break ;
+	  case _GPIOE_PORT  :
+		  SET_BIT(GPIOE_SPTR->GPIO_LCKR.RegisterAccess, (U32)(PinNum)) ; break;
+		  SET_BIT(GPIOE_SPTR->GPIO_LCKR.RegisterAccess, (U32)(16U))    ; break;
+		  while(!(GPIOE_SPTR->GPIO_LCKR.RegisterAccess, (U32)(16U)));
+		  break ;
+	  case _GPIOH_PORT  :
+		  SET_BIT(GPIOH_SPTR->GPIO_LCKR.RegisterAccess, (U32)(PinNum)) ; break;
+		  SET_BIT(GPIOH_SPTR->GPIO_LCKR.RegisterAccess, (U32)(16U))    ; break;
+		  while(!(GPIOH_SPTR->GPIO_LCKR.RegisterAccess, (U32)(16U)));
+		  break ;
+	  default : /*ERROR*/ break ;
+	}
+}
+
+void MGPIO_vSetPinAltFun(U8 Copy_u8PortNum, U8 PinNum, U8 ALTFUN )
+{
+	if(PinNum <=7U)
+	{
+		switch(Copy_u8PortNum )
+		{
+			 case GPIOA_PORT  : GPIOA_SPTR->GPIO_AFRL.RegisterAccess |= (U32)(ALTFUN<<(4U*PinNum)) ; break ;
+			 case GPIOB_PORT  : GPIOB_SPTR->GPIO_AFRL.RegisterAccess |= (U32)(ALTFUN<<(4U*PinNum)) ; break ;
+			 case GPIOC_PORT  : GPIOC_SPTR->GPIO_AFRL.RegisterAccess |= (U32)(ALTFUN<<(4U*PinNum)) ; break ;
+			 case GPIOD_PORT  : GPIOD_SPTR->GPIO_AFRL.RegisterAccess |= (U32)(ALTFUN<<(4U*PinNum)) ; break ;
+			 case GPIOE_PORT  : GPIOE_SPTR->GPIO_AFRL.RegisterAccess |= (U32)(ALTFUN<<(4U*PinNum)) ; break ;
+			 case GPIOH_PORT  : GPIOH_SPTR->GPIO_AFRL.RegisterAccess |= (U32)(ALTFUN<<(4U*PinNum)) ; break ;
+			 default : /*ERROR*/ break ;
+		}
+	}
+	else	// 8 : 15
+	{
+		PinNum %=8;
+		switch(Copy_u8PortNum )
+		{
+			 case GPIOA_PORT  : GPIOA_SPTR->GPIO_AFRH.RegisterAccess |= (U32)(ALTFUN<<(4U*PinNum)) ; break ;
+			 case GPIOB_PORT  : GPIOB_SPTR->GPIO_AFRH.RegisterAccess |= (U32)(ALTFUN<<(4U*PinNum)) ; break ;
+			 case GPIOC_PORT  : GPIOC_SPTR->GPIO_AFRH.RegisterAccess |= (U32)(ALTFUN<<(4U*PinNum)) ; break ;
+			 case GPIOD_PORT  : GPIOD_SPTR->GPIO_AFRH.RegisterAccess |= (U32)(ALTFUN<<(4U*PinNum)) ; break ;
+			 case GPIOE_PORT  : GPIOE_SPTR->GPIO_AFRH.RegisterAccess |= (U32)(ALTFUN<<(4U*PinNum)) ; break ;
+			 case GPIOH_PORT  : GPIOH_SPTR->GPIO_AFRH.RegisterAccess |= (U32)(ALTFUN<<(4U*PinNum)) ; break ;
+			 default : /*ERROR*/ break ;
+		}
+	}
+}
