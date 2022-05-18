4cb48cb959313725e964cee406369082c83fdfac
Mohamedmukhtar1 <muhamdmokhtar1@gmail.com>
2022-05-18 19:36:29 +0200

    OK

---
+++ STM32F401VET6_Drivers/Inc/Utility/APP_Utility.h
@@ -0,0 +1,16 @@
+/*
+ * APP_Utility.h
+ *
+ *  Created on: May 13, 2022
+ *      Author: Eng- Mohamed Mokhtar
+ */
+
+#ifndef INC_APP_UTILITY_APP_UTILITY_H_
+#define INC_APP_UTILITY_APP_UTILITY_H_
+
+//<include statement>				 #include "APP_Utility.h"
+#include "APP_STD_Types.h"
+#include "APP_BIT_MATH.h"
+#include "APP_REG_Types.h"
+
+#endif /* INC_APP_UTILITY_APP_UTILITY_H_ */
